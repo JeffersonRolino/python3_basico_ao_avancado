d8fd6d7b7d5f6a40cc34515d6b23884c6d977f55
jeffersonrolino <jefferson.rolino@gmail.com>
2023-08-29 09:34:28 -0300

    Aula 8 - Exercício com Variáveis

---
+++ aula_8.py
@@ -0,0 +1,13 @@
+nome = "João"
+sobrenome = "Silva"
+idade = 23
+ano_nascimento = 2000
+maior_de_idade = True
+altura_metros = 1.75
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de nascimento:", ano_nascimento)
+print("É maior de idade?:", maior_de_idade)
+print("Altura em metros:", altura_metros)
