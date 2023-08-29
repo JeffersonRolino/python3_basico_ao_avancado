ccccdd97c9afa5496989b8ad1a96b7f19bd471aa
jeffersonrolino <jefferson.rolino@gmail.com>
2023-08-29 07:34:01 -0300

    Aula 3 finalizada

---
+++ aula_3.py
@@ -0,0 +1,17 @@
+"""
+Python = Linguagem de programação
+Tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+# Aspas simples
+print("Jefferson Rolino")
+
+# Aspas duplas
+print("Engenharia de Software")
+
+# Escape
+print("Jefferson \"Rolino\"")
