5730995fc6426ad3e3164b0800f756857c0a4bd6
jeffersonrolino <jefferson.rolino@gmail.com>
2023-08-31 19:54:47 -0300

    Aula 23 - Operador Lógico 'not'

---
+++ aula_23.py
@@ -0,0 +1,16 @@
+# Operador lógico 'not'
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+
+print(not 0)  # True
+print(not 1)  # False
+
+senha = input("Digite sua senha: ")
+
+
+if senha != "123":
+    print("Senha incorreta.")
+else:
+    print("Entrou...")
