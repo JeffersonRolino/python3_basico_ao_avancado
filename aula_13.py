0ef87d6b07e6166138b7d57839ea31f493eca91a
jeffersonrolino <jefferson.rolino@gmail.com>
2023-08-30 23:56:24 -0300

    Aula 13 - Uma introdução às f-strings

---
+++ aula_13.py
@@ -0,0 +1,9 @@
+# IMC = peso / (altura x altura)
+nome = "Fernando Pessoa"
+altura = 1.80
+peso = 95
+imc = peso / altura**2
+
+print(f"{nome} tem {altura:.2f} de altura,")
+print(f"pesa {peso} quilos e seu IMC é")
+print(imc)
