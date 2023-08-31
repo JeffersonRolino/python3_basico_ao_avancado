9dfdbb9755ae342e42829912e9567049753da82b
jeffersonrolino <jefferson.rolino@gmail.com>
2023-08-30 23:49:19 -0300

    Aula 12 - Exercício: Cálculo do IMC (Índice de Massa Corpórea)

---
+++ aula_12.py
@@ -0,0 +1,9 @@
+# IMC = peso / (altura x altura)
+nome = "Fernando Pessoa"
+altura = 1.80
+peso = 95
+imc = peso / altura**2
+
+print(str(nome) + " tem " + str(altura) + " de altura,")
+print("pesa " + str(peso) + " quilos e seu IMC é")
+print(imc)
