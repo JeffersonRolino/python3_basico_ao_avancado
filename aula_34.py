3421411619116c2d9e9f44600d9237d85d9c2399
jeffersonrolino <jefferson.rolino@gmail.com>
2023-09-04 10:37:28 -0300

    Aula 34 - While Loop - Part 1

---
+++ aula_34.py
@@ -0,0 +1,13 @@
+"""
+Repetições
+while(enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+"""
+
+condicao = True
+number = 0
+while condicao:
+    print(number)
+    number += 1
+    if number == 10:
+        condicao = False
