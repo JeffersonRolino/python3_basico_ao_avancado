fb2c02fe35c753b9e61f9769d33cb66e7cbc4f9c
jeffersonrolino <jefferson.rolino@gmail.com>
2023-09-07 12:31:24 -0300

    Aula 49 - for in com tipo list

---
+++ aula_49.py
@@ -0,0 +1,13 @@
+"""
+for in com listas
+"""
+
+lista = ["Maria", "Helena", "Luiz"]
+
+# for funciona com strings
+for letra in "ABC":
+    print(letra)
+
+# for funciona com listas
+for nome in lista:
+    print(nome)
