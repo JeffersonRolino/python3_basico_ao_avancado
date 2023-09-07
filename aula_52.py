419d4bb6e680aceda13f9c42a6c4aafb438719a7
jeffersonrolino <jefferson.rolino@gmail.com>
2023-09-07 12:58:03 -0300

    Aula 52 - Tipo tuple (tuplas)

---
+++ aula_52.py
@@ -0,0 +1,12 @@
+""" 
+Tipo tupla - Uma lista imútavel
+"""
+
+nomes = ("Maria", "João", "José")
+print(nomes[-1])
+print(nomes)
+
+# convertendo lista para tuple
+numeros = [1, 2, 3, 4, 5]
+numeros = tuple(numeros)
+print(numeros, type(numeros))
