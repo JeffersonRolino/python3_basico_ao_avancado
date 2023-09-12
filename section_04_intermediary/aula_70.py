d34f7b61a06f0670d30d66a03d3857eb13f0de7b
jeffersonrolino <jefferson.rolino@gmail.com>
2023-09-12 08:50:16 -0300

    Aula 70 - Retorno de valores em funções 'return'

---
+++ section_04_intermediary/aula_70.py
@@ -0,0 +1,16 @@
+""" 
+Retorno de valores das funções (return)
+"""
+variavel = print("Jefferson")
+print(variavel)
+
+
+def soma(x, y):
+    if x > 10:
+        return 10
+    return x + y
+
+
+number_1 = soma(35, 2)
+number_2 = soma(78, 2)
+print(number_1 + number_2)
