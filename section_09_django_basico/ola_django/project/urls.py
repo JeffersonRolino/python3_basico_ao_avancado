9fcdbd7b9becfd52e8e793b8aeaa35f6f76cdcd3
jeffersonrolino <jefferson.rolino@gmail.com>
2023-10-02 18:17:38 -0300

    Seção 09 - Django Básico - Aula 423: Entendendo VIEWs e o ciclo Request/Response

--- section_09_django_basico/ola_django/project/urls.py
+++ section_09_django_basico/ola_django/project/urls.py
@@ -22,12 +22,18 @@ from django.urls import path
 # MVT (MVC)
 
 
+def home(request):
+    print("HOME")
+    return HttpResponse("HOME")
+
+
 def my_view(request):
-    print("Posso fazer outras coisas")
-    return HttpResponse("Uma mensagem para alguém especial...")
+    print("BLOG")
+    return HttpResponse("BLOG")
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", home),
     path("blog/", my_view),
 ]
